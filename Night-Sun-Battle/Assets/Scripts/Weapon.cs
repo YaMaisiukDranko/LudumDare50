72412cf69a6b2189cd09ccc362aa4018cc1dac42
Yaroslav <yarikmeisan@yandex.com>
2022-04-02 13:17:02 +0300

    Setting up Shoot in Weapon.cs

--- Night-Sun-Battle/Assets/Scripts/Weapon.cs
+++ Night-Sun-Battle/Assets/Scripts/Weapon.cs
@@ -1,18 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    private void Update()
     {
-        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot(); 
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Shoot()
     {
-        
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
     }
 }
