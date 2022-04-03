8887726f7d79e90f40d30d08469b73f85fe17e5b
Yaroslav <yarikmeisan@yandex.com>
2022-04-03 22:10:57 +0300

    Add flipX to enemies

--- LudumDare50_Game/Assets/Scripts/EnemyScript.cs
+++ LudumDare50_Game/Assets/Scripts/EnemyScript.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
@@ -8,10 +9,14 @@ public class EnemyScript : MonoBehaviour
     public Animator _animator;
     private float dirX = 0f;
     private Rigidbody rb;
+    private Transform enemyTransform;
+    private SpriteRenderer enemySprite;
 
     private void Start()
     {
         _animator = GetComponent<Animator>();
+        enemyTransform = GetComponent<Transform>();
+        enemySprite = GetComponent<SpriteRenderer>();
         _animator.SetTrigger("walk");
     }
 
@@ -27,6 +32,18 @@ public class EnemyScript : MonoBehaviour
         if (!other.CompareTag("PlayerTarget"))
         {
             _animator.SetTrigger("walk");
+        }
+    }
+
+    private void Update()
+    {
+        if (enemyTransform.position.x > 0)
+        {
+            enemySprite.flipX = true;
+        }
+        else if (enemyTransform.position.x < 0)
+        {
+            enemySprite.flipX = false;
         }
     }
 }
