498a1c49593ce2be85cbd47e599f1ca8745f4e10
Yaroslav <yarikmeisan@yandex.com>
2022-04-03 17:43:09 +0300

    Setting up Following Player

---
+++ LudumDare50_Game/Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public float speed;
+    public float stoppingDistance;
+    private Transform target;
+    
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("PlayerTarget").GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        if (Vector2.Distance(transform.position, target.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+    }
+}
