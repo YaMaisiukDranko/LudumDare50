b42a7f16418db2817fea78138c2b2b5abb51e799
Yaroslav <yarikmeisan@yandex.com>
2022-04-04 19:03:51 +0300

    Setting up Hit enemy

--- LudumDare50_Game/Assets/Scripts/PlayerAttacks.cs
+++ LudumDare50_Game/Assets/Scripts/PlayerAttacks.cs
@@ -1,18 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAttacks : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform AttackPoint;
+    public float AttackRange = 0.5f;
+    public LayerMask enemyLayers;
+
+    private void Update()
     {
-        
+        Attack(); 
     }
 
-    // Update is called once per frame
-    void Update()
+    void Attack()
     {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(AttackPoint.position, AttackRange, enemyLayers);
+        foreach (Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("we hit " + enemy.name);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if(AttackPoint == null) 
+            return;
         
+        Gizmos.DrawWireSphere(AttackPoint.position, AttackRange);
     }
 }
